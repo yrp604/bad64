ee816be308ce48d99bf6f4d13827542326cf5bdd
yrp <yrp604@protonmail.com>
2021-02-03 18:45:52 -0800

    Update example with new operands

--- examples/rand.rs
+++ examples/rand.rs
@@ -4,8 +4,8 @@ fn main() {
     loop {
         if let Ok(ins) = bad64::decode(rand::thread_rng().gen(), rand::thread_rng().gen()) {
             print!("{:x?}", ins.operation());
-            for n in 0..ins.num_operands() {
-                print!(" {:x?}", ins.operand(n).unwrap());
+            for o in ins.operands() {
+                print!(" {:x?}", o);
             }
             println!("");
         }

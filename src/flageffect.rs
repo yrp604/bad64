7c2d3d0f1728ae1212990edb4c5863d1a4eaa9e7
yrp <yrp604@protonmail.com>
2023-11-13 19:48:19 -0800

    add missing flageffect file

---
+++ src/flageffect.rs
@@ -0,0 +1,23 @@
+/// The semantic meaning of the flag setting
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum FlagEffect {
+    Sets,
+    Integer,
+    Float,
+}
+
+#[doc(hidden)]
+#[allow(non_upper_case_globals)]
+impl TryFrom<&bad64_sys::Instruction> for FlagEffect {
+    type Error = ();
+
+    fn try_from(ii: &bad64_sys::Instruction) -> Result<Self, Self::Error> {
+        match ii.setflags {
+            bad64_sys::FlagEffect::FLAGEFFECT_SETS => Ok(FlagEffect::Sets),
+            bad64_sys::FlagEffect::FLAGEFFECT_SETS_NORMAL => Ok(FlagEffect::Integer),
+            bad64_sys::FlagEffect::FLAGEFFECT_SETS_FLOAT => Ok(FlagEffect::Float),
+            bad64_sys::FlagEffect::FLAGEFFECT_NONE => Err(()),
+        }
+    }
+}
+     

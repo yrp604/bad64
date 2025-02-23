49d7afa7b252b5f7864b67411e61827224a0ac12
yrp <yrp604@protonmail.com>
2025-02-22 17:49:57 -0800

    rand fix

--- examples/rand.rs
+++ examples/rand.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 
 fn main() {
     loop {
-        if let Ok(decoded) = bad64::decode(rand::thread_rng().r#gen(), rand::rng()) {
+        if let Ok(decoded) = bad64::decode(rand::rng().random::<u32>(), rand::rng().random::<u64>()) {
             println!("{:x}: {}", decoded.opcode(), decoded);
         }
     }

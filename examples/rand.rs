e2cd32e016f245411383554afac36e35c980556c
yrp <yrp604@protonmail.com>
2025-02-22 17:42:57 -0800

    2024

--- examples/rand.rs
+++ examples/rand.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 
 fn main() {
     loop {
-        if let Ok(decoded) = bad64::decode(rand::thread_rng().gen(), rand::thread_rng().gen()) {
+        if let Ok(decoded) = bad64::decode(rand::thread_rng().r#gen(), rand::thread_rng().r#gen()) {
             println!("{:x}: {}", decoded.opcode(), decoded);
         }
     }
